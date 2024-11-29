60cf36c061434a627b897aa0ddbd90dc2343e2de
Anna Rosa Klaver <a.r.klaver@students.uu.nl>
2024-11-29 13:48:42 +0100

    Additional data points incorporated

--- data_processor.R
+++ data_processor.R
@@ -3,7 +3,7 @@ library(tidyverse)
 data <- 'assignment2_data_raw.csv' |>
   read_csv() |>
   select(ResponseId, Gender, Nationality, Semester, Safety_C, Protests_C, Protests_EX, Safety_EX) |>
-  slice_tail(n=142) |>
+  slice_tail(n=-2) |>
   mutate(group = case_when(!is.na(Protests_C) ~ 'Control',
                             is.na(Protests_C) ~ 'Experimental')) |>
   unite(col='Safety', Safety_EX, Safety_C, sep='', na.rm = TRUE) |>
